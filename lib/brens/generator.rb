479912eee721d7be16d85cbaf4c73b75b3ddee40
Dan Richert <dan.richert@gmail.com>
2012-06-05 23:35:36 -0400

    wip: Generator

--- lib/brens/generator.rb
+++ lib/brens/generator.rb
@@ -1,13 +1,32 @@
 module Brens
   class Generator
-    def initialize(*texts)
-      @brain = Brain.new
+    def initialize(opts = {})
+      opts = {
+        :phrase_length = 3
+      }.merge(opts)
+
+      @brain         = Brain.new
+      @phrase_length = opts[:phrase_length]
     end
 
     def add_text(str)
-      wlist = Wordlist.new(
-        whitespace_split(str)
-      )
+      words = whitespace_split(str)
+
+      @wlist ||= Wordlist.new(words)
+
+      inputs  = []
+      outputs = []
+
+      # iterate through each word
+      words.each_with_index do |w, ndx|
+        # slice of words @phrase_length-long starting at index
+        inputs  << words[ndx..(ndx + (@phrase_length - 1))]
+
+        # word following the slice
+        outputs << [words[ndx + @phrase_length]]
+      end
+
+      @brain.train(inputs, outputs)
     end
 
     def generate(length)
